716407171bcb695bd82b9255643600d8cd1b3bac
tsukasa-instant <tsukasa.php+1@gmail.com>
2019-08-23 04:17:46 +0900

    login and touhyou

---
+++ pages/quiz.vue
@@ -0,0 +1,46 @@
+<template>
+<div v-if="this.quiz">
+    <p>Q1. {{this.quiz.description}}</p> 
+    <ul>
+        <li v-for="choise in this.quiz.choises">
+            <input v-bind:value="choise" v-on:click="selectQuiz(choise)">
+        </li>
+    </ul> 
+</div>
+<div v-else>
+    aaa
+</div>
+</template>
+
+<script>
+import Vuex from 'vuex'
+export default {
+  head: {
+    titleTemplate: '%s - Nuxt.js',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Meta description' }
+    ]
+  },
+  data() {
+    return {
+      quiz: null
+    }
+  },
+  methods: {
+      getCurrentQuiz() {
+          this.quiz = {choises: ["a", "b", "c", "d"], image: "", description: "まんげまんげ", answer: 2};
+      },
+      selectQuiz(choise) {
+          this.$router.push("/wating_nino")
+      }
+  },
+  created() {
+
+  },
+  mounted() {
+      this.getCurrentQuiz();
+  }
+}
+</script>
---
+++ pages/wating_nino.vue
@@ -0,0 +1,24 @@
+<template>
+
+</template>
+
+<script>
+import Vuex from 'vuex'
+export default {
+  head: {
+    titleTemplate: '%s - Nuxt.js',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Meta description' }
+    ]
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
