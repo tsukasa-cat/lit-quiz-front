287e3c2bc5c09c8b506e5d8478d3459022677306
tsukasa-instant <tsukasa.php+1@gmail.com>
2019-08-24 03:19:53 +0900

    fix

---
+++ pages/display/storack_out.vue
@@ -0,0 +1,56 @@
+<template>
+<div>
+    <ul>
+        <li v-for="(hiragana, index) in this.hiraganas">
+            {{hiragana}}
+        </li>
+    </ul> 
+</div>
+</template>
+
+<script>
+import Vuex from 'vuex'
+import axios from 'axios'
+export default {
+  head: {
+    titleTemplate: '%s - Nuxt.js',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Meta description' }
+    ]
+  },
+  data() {
+    return {
+      quiz: null,
+      hiraganas: ["あ", "い", "う", "え", "お", "か", "き", "く", "け", "こ", "さ", "し", "す", "せ", "そ", "た"]
+    }
+  },
+  methods: {
+      async getCurrentQuiz() {
+          this.quiz = (await axios.get("https://e01b0f377f24.vps.mizucoffee.net/quiz/current")).data;
+      },
+      async getTeams() {
+        // this.teams = (await axios.get("https://e01b0f377f24.vps.mizucoffee.net/team/list")).data;
+        this.teams = [
+          {name: "A", choise: "ちょこ"},
+          {name: "A", choise: "ちょこ"},
+          {name: "A", choise: "ちょこ"},
+          {name: "A", choise: "ちょこ"},
+          {name: "A", choise: "ちょこ"}
+        ];
+        console.log(this.teams);
+      },
+      finishQuiz() {
+          this.$router.push("/display/choises");
+      }
+  },
+  created() {
+
+  },
+  mounted() {
+      this.getCurrentQuiz();
+      this.getTeams();
+  }
+}
+</script>
